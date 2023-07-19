1d0e042785f67deb1abb9b9f1fc8b6e9721b6036
ajay baburaj <ajaybaburajp@gmail.com>
2023-07-19 13:17:18 +0530

    comments removed

--- app.js
+++ app.js
@@ -14,30 +14,6 @@ app.use(express.urlencoded({extended:true}))
 
 
 app.use('/api/product',router)
-// app.get('/api/product/get',(req,res,next)=>{
-//     try{
-//         const data = getAllProducts()
-//         console.log('call is coming here')
-//     }catch(err){
-//         console.log(err.message)
-//     }  
-// })
-
-// app.post('/api/product/save',async (req,res)=>{
-//     const data =  await AddProduct(req.body)
-//     console.log(data)
-// })
-
-// app.get('/api/product/categories',async(req,res)=>{
-//     const data = await getAllCategories()
-//     res.status(200).json(data)
-// })
-
-// app.get("/api/product/list",async(req,res)=>{
-//     const Id = req.query.categoryId
-//     const data = await GetProductsByCategory(Id)
-
-// })
 
 app.all('*',(req,res,next)=>{
     next (new AppError('Not found',404))
